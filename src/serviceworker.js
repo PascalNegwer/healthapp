298cc00afd0043b2f7f0ff2c06be786ee1969cef
Pascal Negwer <pn@koempf.de>
2018-04-26 16:21:36 +0200

    [TASK] improves service-worker caching

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -3,6 +3,18 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox
 let revision = (new Date()).toTimeString();
 let filesToCache = [
   {
+    'url': 'assets/css/main.css',
+    'revision': revision,
+  },
+  {
+    'url': '/0.main.js',
+    'revision': revision,
+  },
+  {
+    'url': '/apiomat.js',
+    'revision': revision,
+  },
+  {
     'url': '/',
     'revision': revision,
   },
@@ -11,34 +23,14 @@ let filesToCache = [
     'revision': revision,
   },
   {
-    'url': '/0.main.js',
-    'revision': revision,
-  },
-  {
-    'url': '/1.main.js',
-    'revision': revision,
-  },
-  {
-    'url': '/2.main.js',
-    'revision': revision,
-  },
-  {
-    'url': '/styles/main.css',
-    'revision': revision,
-  },
-  {
     'url': '/manifest.json',
-    'revision': revision,
-  },
-  {
-    'url': '/com/apiomat/apiomat.js',
     'revision': revision,
   },
 ];
 
-workbox.setConfig({
-  debug: true
-});
-workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
+// workbox.setConfig({
+//   debug: true
+// });
+// workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
 
 workbox.precaching.precacheAndRoute(filesToCache);
