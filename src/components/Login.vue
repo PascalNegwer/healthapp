934b596e91a6d3aaa4bd2e8e430932cc677fa9ec
Philipp <philipp-reg@web.de>
2018-04-26 23:46:31 +0200

    [BUG] Fixed positioning issues for scrolling on phone

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,21 +1,21 @@
 <template>
-    <div class="l_flex l_flex--vertical l_container l_wrapper">
-        <img src="assets/img/logo.svg" class="logo u_center">
-        <div v-if="loading">
-            <h1>Loading</h1>
-        </div>
-        <div v-else>
-            <div v-if="error.message">
-                {{ error.statusCode }}: {{ error.message }}
-            </div>
-            <form class="l_flex l_flex--vertical" v-on:submit.prevent="login">
-                <input class="inp inp--18" v-model="user.data.userName" type="email" placeholder="E-Mail-Adresse" required>
-                <input class="inp inp--18" v-model="user.data.password" type="password" placeholder="Passwort" required>
-                <button class="btn btn--18 u_center" type="submit">Login</button>
-            </form>
-            Noch kein Account? <router-link to="/signup">Hier registrieren!</router-link>
-        </div>
+  <div class="l_flex l_container l_wrapper">
+    <img src="assets/img/logo.svg" class="logo u_center">
+    <div v-if="loading">
+      <h1>Loading</h1>
     </div>
+    <div class="l_flex" v-else>
+      <div v-if="error.message">
+        {{ error.statusCode }}: {{ error.message }}
+      </div>
+      <form class="l_flex" v-on:submit.prevent="login">
+        <input class="inp inp--18" v-model="user.data.userName" type="email" placeholder="E-Mail-Adresse" required>
+        <input class="inp inp--18" v-model="user.data.password" type="password" placeholder="Passwort" required>
+        <button class="btn btn--18 u_center" type="submit">Login</button>
+      </form>
+      <router-link to="/signup" class="link u_center">Noch kein Account?</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -63,11 +63,25 @@
 <style>
   .logo {
     width: 40%;
+    margin-bottom: 20vw;
   }
   .btn {
     width: 65%;
+    margin-top: 20vw;
+    margin-bottom: 20vw;
   }
   .inp {
-    margin: 2rem 0;
+    margin: 1rem 0;
+  }
+  .l_wrapper {
+    justify-content: flex-start;
+  }
+  .link {
+    font-size: 1.8rem;
+    line-height: 2;
+    transition: color .15s ease-in-out;
+  }
+  .link:active {
+    opacity: .5;
   }
 </style>
