3a885f606726ba9492fabc682d437a5f97c0fda8
Flip <philipp-reg@web.de>
2018-04-28 16:02:46 +0200

    [TASK] Added icons to main nav and modified layout/visuals

--- src/components/main/MainNav.vue
+++ src/components/main/MainNav.vue
@@ -1,9 +1,29 @@
 <template>
   <nav class="nav l_flex l_flex--horizontal">
-    <router-link class="nav__item" to="/">Home</router-link>
-    <router-link class="nav__item" to="#">Link1</router-link>
-    <router-link class="nav__item" to="#">Link2</router-link>
-    <router-link class="nav__item" to="#">Link3</router-link>
+    <span class="nav__item">
+      <router-link class="nav__link" to="/">
+        <img class="nav__icon" src="assets/img/home.svg">
+        <p class="nav__text">Home</p>
+      </router-link>
+    </span>
+    <span class="nav__item">
+      <router-link class="nav__link" to="#">
+        <img class="nav__icon" src="assets/img/dashboard.svg">
+        <p class="nav__text">Dashboard</p>
+      </router-link>
+    </span>
+    <span class="nav__item">
+      <router-link class="nav__link" to="#">
+        <img class="nav__icon" src="assets/img/list.svg">
+        <p class="nav__text">Funktionen</p>
+      </router-link>
+    </span>
+    <span class="nav__item nav__item--dropdown">
+      <div class="nav__link">
+        <img class="nav__icon" src="assets/img/settings.svg">
+        <p class="nav__text">Settings</p>
+      </div>
+    </span>
   </nav>
 </template>
 
@@ -21,10 +41,27 @@
     bottom: 0;
     left: 0;
     justify-content: space-between;
+    flex-wrap: nowrap;
   }
   .nav__item {
+    width: 25%;
     display: block;
     text-align: center;
-    padding: 2rem;
+    padding-bottom: 2rem;
+    transition: background .15s ease-in-out;
+  }
+  .nav__item:active {
+    background: var(--white-50);
+  }
+  .nav__icon {
+    padding: 2rem 3rem 1rem 3rem;
+    width: 100%;
+    height: auto;
+  }
+  .nav__text {
+    font-size: 1.2rem;
+    font-weight: 400;
+    text-transform: uppercase;
+    text-align: center;
   }
 </style>
