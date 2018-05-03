2e6e58d632aad4f43a2ddf5a73960185bdd51164
Miriam Andriof <miriam.andriof@freenet.de>
2018-05-03 11:17:57 +0200

    law-text and first styles

--- src/components/main/Lawstuff.vue
+++ src/components/main/Lawstuff.vue
@@ -1,6 +1,39 @@
 <template>
     <div class="l_wrapper l_wrapper--small">
-        <h1>lawstuff</h1>
+        <div class='impressum'>
+            <h1 class="headline headline--big">Impressum</h1>
+            <br>
+            <p class="paragraph">Angaben gemäß § 5 TMG
+            <br><br>
+            DHBW Mosbach <br>
+                Oberer Mühlenweg 2-6<br>
+                74821 Mosbach<br>
+            <br>
+            <strong>Vertreten durch: </strong><br><br>
+            Miriam Andriof<br>
+            Linda Jansen<br>
+            Philipp Kuhn<br>
+            Pascal Negwer<br>
+            Lea-Sophie Rother<br>
+            <br>
+            <strong>Kontakt:</strong> <br><br>
+            Telefon: 01234-789456<br>
+            E-Mail: <a href='mailto:wt5health@gmail.com'>wt5health@gmail.com</a>
+            <br></p>
+            <br>
+            <br>
+            <h3 class="headline headline--small">Haftung für Inhalte</h3>
+            <br>
+            <p class="paragraph">Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
+            <br>
+            <h3 class="headline headline--small">Datenschutz</h3>
+            <br>
+            <p class="paragraph">Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben. <br>
+                Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich. <br>
+                Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.<br>
+            </p>
+        </div>
+
     </div>
 </template>
 
@@ -20,5 +53,5 @@
   }
 </script>
 
-<style>
+<style scoped>
 </style>
