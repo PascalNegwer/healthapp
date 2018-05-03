662b8bb322e6291eea4f882cf59bd3a14eb41c9c
Miriam Andriof <miriam.andriof@freenet.de>
2018-05-03 18:03:03 +0200

    first layout

--- src/components/main/Account.vue
+++ src/components/main/Account.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="l_wrapper l_wrapper--small">
-        <h1>account</h1>
+        <div class="l_flex content">
+        <h1 class="headline headline--h1">Accountdaten</h1>
+        <p class="description--inputbox">Name</p>
+        <input class="inp inp--18" placeholder="Name">
+        <p class="description--inputbox">Vorname</p>
+        <input class="inp inp--18" placeholder="Vorname">
+        <p class="description--inputbox">E-Mail</p>
+        <input class="inp inp--18" v-model="$user.data.userName" type="email" placeholder="E-Mail-Adresse" required>
+        <button class="btn btn--18" type="submit">Speichern</button>
+        </div>
     </div>
 </template>
 
@@ -20,5 +29,17 @@
   }
 </script>
 
-<style>
+<style scoped>
+    .btn {
+        width: 65%;
+        margin-top: 4vh;
+        margin-bottom: 4vh;
+        margin-left: auto;
+    }
+    .inp {
+        margin: 1rem 0 2rem 0;
+    }
+    .headline {
+        margin-bottom: 2.5rem;
+    }
 </style>
