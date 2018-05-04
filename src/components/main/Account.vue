25a790a08b60bb4ae5f9091d64a2d6269b5e5959
Miriam Andriof <miriam.andriof@freenet.de>
2018-05-04 14:35:40 +0200

    [TASK] change password input fields and submit button

--- src/components/main/Account.vue
+++ src/components/main/Account.vue
@@ -3,17 +3,29 @@
     <h1 class="headline headline--main">Account</h1>
     <section class="l_section">
       <h2 class="headline">Accountdaten</h2>
-      <form class="l_flex">
+      <form class="l_flex" v-on:submit.prevent="save">
         <p class="label">Name</p>
         <input class="inp" v-model="$user.data.lastName" placeholder="Name">
-        <p class="description--inputbox">Vorname</p>
+        <p class="label">Vorname</p>
         <input class="inp" v-model="$user.data.firstName" placeholder="Vorname">
-        <p class="description--inputbox">E-Mail</p>
+        <p class="label">E-Mail</p>
         <input class="inp" v-model="$user.data.userName" type="email" placeholder="E-Mail-Adresse" readonly>
         <button class="btn" type="submit">Speichern</button>
       </form>
     </section>
     <span class="l_divider"></span>
+      <section class="l_section">
+          <h2 class="headline">Passwort ändern</h2>
+          <form class="l_flex" v-on:submit.prevent="savePassword">
+              <p class="label">Altes Passwort</p>
+              <input class="inp" placeholder="Altes Passwort" type="password" required>
+              <p class="label">Neues Passwort</p>
+              <input class="inp" placeholder="Neues Passwort" type="password" required>
+              <p class="label">Neues Passwort wiederholen</p>
+              <input class="inp" placeholder="Passwort wiederholen" type="password" required>
+              <button class="btn" type="submit">Passwort ändern</button>
+          </form>
+      </section>
   </div>
 </template>
 
@@ -56,6 +68,9 @@
                         }
                     },
                 });
+            },
+            savePassword: function () {
+
             }
         }
     }
