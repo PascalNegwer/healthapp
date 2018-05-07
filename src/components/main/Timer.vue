3365839da581a682f0d4d2649181ce928892b8ef
Flip <philipp-reg@web.de>
2018-05-07 18:14:32 +0200

    [FEATURE] Force portrait mode display

--- src/components/main/Timer.vue
+++ src/components/main/Timer.vue
@@ -146,6 +146,9 @@
     color: var(--white-50);
     margin-bottom: .6rem;
   }
+  .timer__label span {
+    display: inline-block;
+  }
   .timer__count {
     font-family: 'Roboto', sans-serif;
     font-weight: 300;
