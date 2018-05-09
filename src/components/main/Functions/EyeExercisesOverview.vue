e8ce0e7af833156e1dedfe68975f705cd42f1f1a
Miriam Andriof <miriam.andriof@freenet.de>
2018-05-09 15:41:10 +0200

    [TASK] design list items EyeOverview

--- src/components/main/Functions/EyeExercisesOverview.vue
+++ src/components/main/Functions/EyeExercisesOverview.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="l_wrapper l_wrapper--small">
-    <h1>Augenübungen</h1>
+    <h1 class="headline headline--main">Augenübungen</h1>
     <ul>
-      <li v-for="eyeExercise in eyeExercises">
-        <router-link v-bind:eyeExercise="eyeExercise" class="" :to="{ name: 'eye-exercise', params: { id: eyeExercise.data.id }}">
+      <li class="list-item" v-for="eyeExercise in eyeExercises">
+        <router-link v-bind:eyeExercise="eyeExercise" class="eyelink-style u_icon--down l_flex l_flex--horizontal" :to="{ name: 'eye-exercise', params: { id: eyeExercise.data.id }}">
           {{ eyeExercise.getTitle() }}
         </router-link>
       </li>
@@ -40,5 +40,27 @@
   }
 </script>
 
+
 <style scoped>
+  .eyelink-style {
+    font-family: Comfortaa, sans-serif;
+    font-size: 1.6rem;
+    justify-content: space-between;
+    padding-right: 14%;
+    align-items: center;
+    transition: opacity .15s ease-in-out;
+  }
+  .eyelink-style:active, .eyelink-style:active:before {
+    opacity: .5;
+  }
+  .eyelink-style:before {
+    transition: opacity .15s ease-in-out;
+    order: 1;
+    transform: rotate(-90deg);
+    font-size: 2rem;
+  }
+  .list-item {
+    width: 100%;
+  }
+
 </style>
